1d8551757493280fa42585707a2542948e2dfad1
Saulo <saulo679@gmail.com>
2021-08-17 20:09:24 -0300

    Atualização DAO.

--- src/main/java/pc2/lab/aula09/dao/FigurasGeometricasDAO.java
+++ src/main/java/pc2/lab/aula09/dao/FigurasGeometricasDAO.java
@@ -6,12 +6,18 @@ import java.io.*;
 
 public class FigurasGeometricasDAO {
 
-    public void salvarEmArquivos(FiguraGeometrica[] fig){
-
+    public void salvarEmArquivos(FiguraGeometrica[] fig) {
+        FileOutputStream arquivo = new FileOutputStream("figurasGeometrica.dat");
+        ObjectOutputStream out = new ObjectOutputStream(arquivo);
+        out.writeObject(fig);
     }
 
     public FiguraGeometrica[]  carregarDeArquivo(){
         FiguraGeometrica[] fig = null;
+
+        FileInputStream arqv = new FileInputStream("f igurasGeometrica.dat")
+        ObjectInputStream in = new ObjectInputStream(arqv);
+        fig = (FiguraGeometrica[])in.readObject();
         return fig;
     }
 }
